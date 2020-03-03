0e0974b4f39369ea605a82c3f6ee9e09052a2f88
Jacob Youssif <103043338@student.swin.edu.au>
2020-03-03 14:07:37 +1100

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace introprogselection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = 18;
+            int legalAge = 18;
+
+            if (age >= legalAge)
+            {
+                Console.WriteLine("DRINK");
+                Console.WriteLine("DRIVE");
+            }
+            else
+            {
+                Console.WriteLine("DON'T DRINK");
+                Console.WriteLine("DON'T DRIVE WITHOUT THE L PLATE");
+            }
+            Console.WriteLine("PROGRAM FINISHED");
+        }
+    }
+}
